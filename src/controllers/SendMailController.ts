48ede87e1d1d712ec705892518fcc1ac85dab399
Jeniffer Marques <genes.marques.silva@gmail.com>
2021-02-27 15:07:00 -0300

    adicionando funcionalidades de envio de email

---
+++ src/controllers/SendMailController.ts
@@ -0,0 +1,47 @@
+import { request } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/SurveysRepository";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+
+class SendMailController {
+
+    async execute(request, response) {
+
+        const { email, survey_id} = request.body;
+
+        const usersRepository = getCustomRepository(UsersRepository);
+        const surveysRepository = getCustomRepository(SurveysRepository);        
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const userAlreadyExists = await usersRepository.findOne({email})
+
+        if(!userAlreadyExists) {
+            return response.stats(400).json({
+                error: "User does not exists",
+            });
+        }
+        const surveyAlreadyExists = await surveysRepository.findOne({id: survey_id})
+
+        if(!surveyAlreadyExists) {
+            return response.status(400).json({
+                error: "survey does not exists!"
+            })
+        }
+
+        // Salvar as informações na tavela surveyUser
+        const surveyUser = surveysUsersRepository.create({
+            user_id: userAlreadyExists.id,
+            survey_id
+        })
+
+        await surveysUsersRepository.save(surveyUser);
+        /// Enviar e-mail para o usuário
+
+        return response.json(surveyUser)
+    }
+
+}
+
+export { SendMailController }
