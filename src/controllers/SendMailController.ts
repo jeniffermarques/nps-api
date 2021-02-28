4e2a8c83e14882368ae9ac3702c1a9ea8e92a1d4
Jeniffer Marques <genes.marques.silva@gmail.com>
2021-02-28 16:32:26 -0300

    mais funcionalizades no envio de email

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -32,6 +32,28 @@ class SendMailController {
             })
         }
 
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            user_id: user.id,
+            link: process.env.URL_MAIL        
+        };
+
+        const npsPath  = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");       
+
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: [{user_id: user.id}, {value: null}]
+        })
+        
+
+        if(surveyUserAlreadyExists) {
+            await SendMailService.execute(email, survey.title, variables, npsPath)
+            
+            return response.json(surveyUserAlreadyExists)
+        }
+
         // Salvar as informações na tavela surveyUser
         const surveyUser = surveysUsersRepository.create({
             user_id: user.id,
@@ -41,15 +63,6 @@ class SendMailController {
         await surveysUsersRepository.save(surveyUser);
         
         /// Enviar e-mail para o usuário
-
-        const npsPath  = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
-
-        const variables = {
-            name: user.name,
-            title: survey.title,
-            description: survey.description
-        }
-
         await SendMailService.execute(email, survey.title, variables, npsPath);
         
         return response.json(surveyUser)
